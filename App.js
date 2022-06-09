e6908b9d9bd518d0b78c579eaf8b3e000f58a3ad
pedroerath <pverath@gmail.com>
2022-06-09 20:53:59 -0300

    corpo criado

--- App.js
+++ App.js
@@ -4,26 +4,81 @@ import {
   View,
   Text,
   TextInput,
-  StyleSheet
+  StyleSheet,
+  Image
 } from 'react-native'
-import Icon from 'react-native-vector-icons/Entypo'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
 
 export default class App extends Component {
   render() {
     return (
       <ScrollView>
-        <View style={estilo.header}>
-          <View style={estilo.item2header}>
-            <Icon name='menu' size={40} color={'white'} />
+        <View style={estilo.item1header}>
+          <TextInput placeholder='🔍Buscar' placeholderTextColor={'black'} style={estilo.textinput} />
+          <Text style={estilo.texto}>🛒Carrinho</Text>
+          <Text style={estilo.texto}>Login</Text>
+        </View>
+        <View style={estilo.item1header}>
+          <Text style={estilo.texto}>Novidade</Text>
+          <Text style={estilo.texto}>Promoções</Text>
+          <Text style={estilo.texto}>Feminina</Text>
+          <Text style={estilo.texto}>Masculino</Text>
+        </View>
+        <View style={{ borderBottomWidth: 1 }}>
+          <Text style={estilo.titulo}>BAZZAAR</Text>
+        </View>
+        <View style={estilo.imagem}>
+          <Image source={require('./src/images/banner.jpeg')} style={{width: wp('95%')}} />
+        </View>
+        <View>
+          <Text style={estilo.titulo2}>Moda Feminina</Text>
+        </View>
+        <View style={estilo.viewprodutos}>
+          <View style={estilo.produtos}>
+            <Image source={require('./src/images/banner.jpeg')} style={{width: wp('20%')}} />
+            <Text style={estilo.texto2}>Blusa</Text>
+            <Text style={estilo.texto2}>R$39,99</Text>
           </View>
-          <View style={estilo.item1header}>
-            <Text style={estilo.texto}>Login</Text>
-            <Text style={estilo.texto}>🛒Carrinho</Text>
-            <TextInput placeholder='🔍Buscar' placeholderTextColor={'black'} style={estilo.textinput} />
+          <View style={estilo.produtos}>
+            <Image source={require('./src/images/banner.jpeg')} style={{width: wp('20%')}} />
+            <Text style={estilo.texto2}>Blusa</Text>
+            <Text style={estilo.texto2}>R$39,99</Text>
+          </View>
+          <View style={estilo.produtos}>
+            <Image source={require('./src/images/banner.jpeg')} style={{width: wp('20%')}} />
+            <Text style={estilo.texto2}>Blusa</Text>
+            <Text style={estilo.texto2}>R$39,99</Text>
+          </View>
+          <View style={estilo.produtos}>
+            <Image source={require('./src/images/banner.jpeg')} style={{width: wp('20%')}} />
+            <Text style={estilo.texto2}>Blusa</Text>
+            <Text style={estilo.texto2}>R$39,99</Text>
           </View>
         </View>
-        <View style={{borderBottomWidth:1}}>
-          <Text style={estilo.titulo}>BAZZAAR</Text>
+        <View>
+          <Text style={estilo.titulo2}>Moda Masculina</Text>
+        </View>
+        <View style={estilo.viewprodutos}>
+          <View style={estilo.produtos}>
+            <Image source={require('./src/images/banner.jpeg')} style={{width: wp('20%')}} />
+            <Text style={estilo.texto2}>Blusa</Text>
+            <Text style={estilo.texto2}>R$39,99</Text>
+          </View>
+          <View style={estilo.produtos}>
+            <Image source={require('./src/images/banner.jpeg')} style={{width: wp('20%')}} />
+            <Text style={estilo.texto2}>Blusa</Text>
+            <Text style={estilo.texto2}>R$39,99</Text>
+          </View>
+          <View style={estilo.produtos}>
+            <Image source={require('./src/images/banner.jpeg')} style={{width: wp('20%')}} />
+            <Text style={estilo.texto2}>Blusa</Text>
+            <Text style={estilo.texto2}>R$39,99</Text>
+          </View>
+          <View style={estilo.produtos}>
+            <Image source={require('./src/images/banner.jpeg')} style={{width: wp('20%')}} />
+            <Text style={estilo.texto2}>Blusa</Text>
+            <Text style={estilo.texto2}>R$39,99</Text>
+          </View>
         </View>
       </ScrollView>
     )
@@ -31,37 +86,56 @@ export default class App extends Component {
 }
 
 const estilo = StyleSheet.create({
-  header: {
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-    backgroundColor: 'gray',
-  },
   item1header: {
+    backgroundColor: '#242526',
     flexDirection: 'row',
-    justifyContent: 'flex-end',
+    justifyContent: 'space-evenly',
     alignItems: 'center',
-  },
-  item2header: {
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
+    borderBottomWidth: 1
   },
   texto: {
-    padding: 5,
+    fontSize:12,
+    padding: 10,
     color: 'white',
+    fontWeight: 'bold'
+  },
+  texto2: {
+    fontSize:15,
+    color: 'black',
     fontWeight: 'bold'
   },
   titulo: {
     fontWeight: 'bold',
-    fontSize: 35,
+    fontSize: 25,
     color: 'black',
-    margin:20,
+    margin: 20,
+  },
+  titulo2: {
+    fontSize: 20,
+    color: 'black',
+    margin: 20,
   },
   textinput: {
-    width: 80,
+    width: hp('23%'),
     margin: 4,
     backgroundColor: 'white',
     borderWidth: 1,
-
+  },
+  imagem:{
+    alignItems: 'center',
+    padding:10,
+  },
+  viewprodutos:{
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    margin:10
+  },
+  produtos:{
+    padding:10,
+    alignItems:'center'
+  },
+  imagemprodutos:{
+    height:50,
+    width: 50
   }
 })
