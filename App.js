980b4bcb0efee887d53640092a467ebf0f8a0db8
pedroerath <pverath@gmail.com>
2022-06-14 19:57:24 -0300

    Revert "Ajustando de acordo com o solicitado"
    This reverts commit 357c56dcf461a2964622b1128991e9e86f45bd41.

--- App.js
+++ App.js
@@ -24,29 +24,33 @@ export default class App extends Component {
           <Text style={estilo.texto}>Feminina</Text>
           <Text style={estilo.texto}>Masculino</Text>
         </View>
-        <View style={estilo.viewInicio}>
+        <View style={{ borderBottomWidth: 1 }}>
           <Text style={estilo.titulo}>BAZZAAR</Text>
-          <Text style={estilo.texto3}>Nossos produtos são inspirados pelas pessoas que estão à nossa volta, com suas belezas e qualidades. Produtos de bom gosto especialmente para você.</Text>
         </View>
         <View style={estilo.imagem}>
-          <Image source={require('./src/images/banner.jpeg')} style={{ width: wp('95%') }} />
+          <Image source={require('./src/images/banner.jpeg')} style={{width: wp('95%')}} />
         </View>
         <View>
           <Text style={estilo.titulo2}>Moda Feminina</Text>
         </View>
         <View style={estilo.viewprodutos}>
           <View style={estilo.produtos}>
-            <Image source={require('./src/images/banner.jpeg')} style={{ width: wp('25%') }} />
+            <Image source={require('./src/images/banner.jpeg')} style={{width: wp('20%')}} />
             <Text style={estilo.texto2}>Blusa</Text>
             <Text style={estilo.texto2}>R$39,99</Text>
           </View>
           <View style={estilo.produtos}>
-            <Image source={require('./src/images/banner.jpeg')} style={{ width: wp('25%') }} />
+            <Image source={require('./src/images/banner.jpeg')} style={{width: wp('20%')}} />
             <Text style={estilo.texto2}>Blusa</Text>
             <Text style={estilo.texto2}>R$39,99</Text>
           </View>
           <View style={estilo.produtos}>
-            <Image source={require('./src/images/banner.jpeg')} style={{ width: wp('25%') }} />
+            <Image source={require('./src/images/banner.jpeg')} style={{width: wp('20%')}} />
+            <Text style={estilo.texto2}>Blusa</Text>
+            <Text style={estilo.texto2}>R$39,99</Text>
+          </View>
+          <View style={estilo.produtos}>
+            <Image source={require('./src/images/banner.jpeg')} style={{width: wp('20%')}} />
             <Text style={estilo.texto2}>Blusa</Text>
             <Text style={estilo.texto2}>R$39,99</Text>
           </View>
@@ -56,26 +60,25 @@ export default class App extends Component {
         </View>
         <View style={estilo.viewprodutos}>
           <View style={estilo.produtos}>
-            <Image source={require('./src/images/banner.jpeg')} style={{ width: wp('25%') }} />
+            <Image source={require('./src/images/banner.jpeg')} style={{width: wp('20%')}} />
             <Text style={estilo.texto2}>Blusa</Text>
             <Text style={estilo.texto2}>R$39,99</Text>
           </View>
           <View style={estilo.produtos}>
-            <Image source={require('./src/images/banner.jpeg')} style={{ width: wp('25%') }} />
+            <Image source={require('./src/images/banner.jpeg')} style={{width: wp('20%')}} />
             <Text style={estilo.texto2}>Blusa</Text>
             <Text style={estilo.texto2}>R$39,99</Text>
           </View>
           <View style={estilo.produtos}>
-            <Image source={require('./src/images/banner.jpeg')} style={{ width: wp('25%') }} />
+            <Image source={require('./src/images/banner.jpeg')} style={{width: wp('20%')}} />
             <Text style={estilo.texto2}>Blusa</Text>
             <Text style={estilo.texto2}>R$39,99</Text>
           </View>
-        </View>
-        <View style={estilo.footer}>
-          <Image source={{ uri: 'https://cdn-icons-png.flaticon.com/512/220/220790.png'}} style={estilo.imagem2} />
-          <Image source={{ uri: 'https://cdn-icons-png.flaticon.com/512/87/87390.png'}} style={estilo.imagem2} />
-          <Image source={{ uri: 'https://cdn-icons-png.flaticon.com/512/1384/1384033.png'}} style={estilo.imagem2} />
-          <Image source={{ uri: 'https://cdn-icons-png.flaticon.com/512/20/20673.png'}} style={estilo.imagem2} />
+          <View style={estilo.produtos}>
+            <Image source={require('./src/images/banner.jpeg')} style={{width: wp('20%')}} />
+            <Text style={estilo.texto2}>Blusa</Text>
+            <Text style={estilo.texto2}>R$39,99</Text>
+          </View>
         </View>
       </ScrollView>
     )
@@ -91,30 +94,21 @@ const estilo = StyleSheet.create({
     borderBottomWidth: 1
   },
   texto: {
-    fontSize: 12,
+    fontSize:12,
     padding: 10,
     color: 'white',
     fontWeight: 'bold'
   },
-  texto3: {
-    flex:5,
-    fontSize: 15,
-    padding: 10,
-    color: 'black',
-    margin:5,
-    textAlign: 'justify'
-  },
   texto2: {
-    fontSize: 15,
+    fontSize:15,
     color: 'black',
     fontWeight: 'bold'
   },
-  titulo:{
-    flex:3,
+  titulo: {
     fontWeight: 'bold',
     fontSize: 25,
     color: 'black',
-    margin:5
+    margin: 20,
   },
   titulo2: {
     fontSize: 20,
@@ -127,35 +121,21 @@ const estilo = StyleSheet.create({
     backgroundColor: 'white',
     borderWidth: 1,
   },
-  imagem: {
+  imagem:{
     alignItems: 'center',
-    padding: 10,
+    padding:10,
   },
-  viewprodutos: {
+  viewprodutos:{
     flexDirection: 'row',
     justifyContent: 'space-evenly',
-    margin: 10
+    margin:10
   },
-  produtos: {
-    padding: 10,
-    alignItems: 'center'
+  produtos:{
+    padding:10,
+    alignItems:'center'
   },
-  imagemprodutos: {
-    height: 50,
-    width: 50
-  },
-  viewInicio:{
-    borderBottomWidth: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    alignItems: 'center'
-  },
-  footer:{
-    borderTopWidth: 1,
-    alignItems: ''
-  },
-  imagem2:{
+  imagemprodutos:{
     height:50,
-    width: 50,
+    width: 50
   }
 })
